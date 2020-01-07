df2f1df5bca75e2e50fc2b7a65483e7faba2d017
Robert Simon <sirob0210@gmail.com>
2020-01-07 12:33:44 +0100

    Basic fertig

---
+++ 01_Basics/03_header.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void Output(int i){
+
+
+    printf("This is from the function file: %i \n", i);
+}
---
+++ 01_Basics/03_main.c
@@ -0,0 +1,15 @@
+/**
+ *  @author: Robert Simon
+ * 
+ *  @title: how to use multiple files
+ */ 
+
+#include "03_header.h"      //the header 
+
+int main(){
+
+    Output(400);
+
+
+    return 0;
+}
